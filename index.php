13d20a4fe7bbb492cdd62626f5cb846b26f46dae
ragakadir123 <irwan.pradana344@gmail.com>
2024-03-20 08:51:02 +0700

    Menambahkan file paragraf

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A sequi blanditiis animi expedita esse, dignissimos natus est modi iusto sit quibusdam at. Fugit esse tempore natus earum ullam ex laudantium.</p>
+</body>
+
+</html>
